f71035ff9af00e252b3586db129ae7e073a7228a
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2019-11-16 11:32:56 +0100

    Add project files.

---
+++ WFA191116/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WFA191116
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl.Location = new System.Drawing.Point(35, 380);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(162, 39);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "Cicaaaaa";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WFA191116.Properties.Resources.cica;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(282, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 224);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WFA191116/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//!!!!
+//using WFA191116.Properties;
+
+namespace WFA191116
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            //button1.BackgroundImage = Properties.Resources.kutya;
+            button1.BackgroundImage = Image.FromFile("delfin.jpg");
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = Properties.Resources.cica;
+        }
+
+    /*
+         vagy MouseEnter vagy MouseHover (inkább enter)
+
+        1) kell kép, amire váltasz
+        2) fontos, hogy a képet úgy nevezd el, hogy a neve változónév lesz!
+        3) be kell másolni a képet a /Properties/Recourses-be
+    	-> Project fül 
+    	-> legalul [projekt neve] properties 
+    	-> felugró ablakban bel oldali menüben recourses (források)
+    	(ha ide másolsz valamit, hozzáad a projet szerkezetéhez egy mabbát, amiben a programban használt forráselemek lesznek majd
+    	(ikonok, stringek, képek, file-ok stb...)
+	
+    	fontos elmenteni az "ablakot" bezárás előtt
+	
+        4) annak a controlnak (most a gomb) a képtulajdonságait úgy kell beállítani, hogy az jó legyen ->
+        alapértelmezés, hogy a képet a bal felsőbe illeszti
+        (egy a BackgrounkImageLayout property)
+
+        :(
+    */
+    }
+}
